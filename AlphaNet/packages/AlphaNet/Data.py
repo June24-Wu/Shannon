4583ac2ec4adf0f0230dc660e0226aa774cbbb4b
June24-Wu <932187556@qq.com>
2022-01-10 14:56:49 +0800

    1.10 afternoon

--- AlphaNet/packages/AlphaNet/Data.py
+++ AlphaNet/packages/AlphaNet/Data.py
@@ -5,6 +5,7 @@ import torch
 import DataAPI
 import os
 import Research.utils.namespace as namespace
+from tqdm import tqdm
 from Research.feature.ft import FeatureAnalysis
 config_path = r'/home/ShareFolder/lgc/Modules/Research/config/feature_bt_template'
 print('Loading the configuration from ' + config_path)
@@ -44,13 +45,43 @@ def generate_original_data(alpha_name, alpha_list, target, start_date, end_date)
     configs = namespace.load_namespace(config_path)
     FT = FeatureAnalysis(configs, feature_path=r"/home/ShareFolder/feature_platform")
     dataloader = DataLoader(None)
-    dataloader.load_data_from_file(data_path="/home/wuwenjun/feature_platform/ti0/wuwenjun/%s/" % (target),
-                                   start_date=start_date, end_date=end_date)
+    dataloader.load_data_from_file(alpha_name = target,
+                                    data_path="/home/wuwenjun/feature_platform/ti0/wuwenjun/",
+                                    start_date=start_date, end_date=end_date)
     dataloader.feature_data.rename(columns={target: "target"}, inplace=True)
     FT.load_feature_from_file(alpha_list, start_date, end_date, universe='Investable', timedelta=None)
     output = pd.concat([FT.feature_data, dataloader.feature_data], axis=1)
     convert_to_standard_daily_data_par(df=output, output_name=alpha_name, output_path="/home/wuwenjun/Data_lib/ti0/wuwenjun/")
     return output
+
+
+def generate_shift_data(alpha_name, shift, data_path="/home/wuwenjun/Data_lib/ti0/wuwenjun/"):
+    dataloader = DataLoader()
+    dataloader.load_data_from_file(alpha_name=alpha_name, data_path=data_path, end_date="2022-01-01")
+
+    # generate shift list
+    sequence_list = [0]
+    for i in range(shift - 1, 30, shift):
+        sequence_list.append(i)
+    sequence_list.sort(reverse=True)
+
+    # shift
+    x = dataloader.feature_data.drop("target", axis=1)
+    final_df = []
+    for index, group in tqdm(x.groupby("ticker")):
+        group_df = []
+        for i in sequence_list:
+            a = group.shift(i)
+            a.columns = [str(j) + "_Shift_%i" % i for j in group.columns]
+            group_df.append(a)
+        group_df = pd.concat(group_df, axis=1)
+        final_df.append(group_df)
+    final_df = pd.concat(final_df, axis=0)
+    final_df = pd.concat([final_df, dataloader.feature_data["target"]], axis=1)
+    final_df.dropna(axis=0,inplace=True)
+    convert_to_standard_daily_data_par(df=final_df, output_name=alpha_name + "_Shift_%i" % shift,
+                                       output_path="/home/wuwenjun/Data_lib/ti0/wuwenjun/")
+    return final_df
 
 
 class DataLoader(object):
@@ -60,11 +91,11 @@ class DataLoader(object):
         self.length = None
         self.y = None
 
-    def load_data_from_file(self,data_path, end_date, start_date="2015-01-01"):
+    def load_data_from_file(self,alpha_name, end_date,data_path = "/home/wuwenjun/Data_lib/ti0/wuwenjun/", start_date="2015-01-01"):
         time_list = DataAPI.get_trading_days(start_date, end_date)
         final = []
-        for date in time_list:
-            date = data_path + str(date.year) + '/' + str(date.year) + str(date.month).zfill(2) + str(date.day).zfill(
+        for date in tqdm(time_list):
+            date = data_path + alpha_name + "/" + str(date.year) + '/' + str(date.year) + str(date.month).zfill(2) + str(date.day).zfill(
                 2) + ".par"
             if (os.path.exists(date)):
                 final.append(pd.read_parquet(date))
